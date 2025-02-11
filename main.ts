57c8155938d9a8d034461324a8119d5354ebbf47
iltomsky <nathan.arnouts2012@gmail.com>
2025-02-11 14:41:05 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,23 +1,31 @@
 Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.On)
+basic.showLeds(`
+    # . . . #
+    . # . # .
+    . . # . .
+    . # . # .
+    # . . . #
+    `)
 basic.forever(function () {
     if (input.buttonIsPressed(Button.B)) {
         Kitronik_STOPbit.trafficLightState(Kitronik_STOPbit.LightStates.Go)
-        basic.pause(5000)
-        basic.showArrow(ArrowNames.West)
         Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Green, Kitronik_STOPbit.DisplayLights.On)
+        basic.showArrow(ArrowNames.West)
+        basic.pause(2000)
         basic.clearScreen()
         Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Green, Kitronik_STOPbit.DisplayLights.Off)
-        basic.showLeds(`
-            . . # . .
-            . . # . .
-            . . # . .
-            . . # . .
-            . . # . .
-            `)
         Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Yellow, Kitronik_STOPbit.DisplayLights.On)
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            `)
+        basic.pause(2000)
         basic.clearScreen()
-        basic.pause(5000)
         Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Yellow, Kitronik_STOPbit.DisplayLights.Off)
+        Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.On)
         basic.showLeds(`
             # . . . #
             . # . # .
@@ -25,7 +33,5 @@ basic.forever(function () {
             . # . # .
             # . . . #
             `)
-        Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.On)
-        basic.clearScreen()
     }
 })
